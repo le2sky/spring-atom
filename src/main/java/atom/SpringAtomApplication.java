52b0b0819cb45ab3006bf25103417c2da65e2917
le2sky <leehaneul990623@gmail.com>
2024-08-12 22:59:40 +0900

    init: 초기 설정

---
+++ src/main/java/atom/SpringAtomApplication.java
@@ -0,0 +1,12 @@
+package atom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAtomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAtomApplication.class, args);
+    }
+}

002c8311b2c8b2e8e386245be117e9bd91e784b4
le2sky <leehaneul990623@gmail.com>
2024-08-22 20:24:20 +0900

    init: 매일 메일 초기화

---
+++ pet-maeil-mail/src/main/java/maeilmail/PetMaeilMailApplication.java
@@ -0,0 +1,12 @@
+package maeilmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMaeilMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMaeilMailApplication.class, args);
+	}
+}

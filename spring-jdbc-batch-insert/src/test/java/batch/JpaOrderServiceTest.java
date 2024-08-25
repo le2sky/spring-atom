74c276e45eeb67022bec99128a0840b7400e0ced
le2sky <leehaneul990623@gmail.com>
2024-08-25 22:41:49 +0900

    feat: jpa batch insert 시도

--- spring-jdbc-batch-insert/src/test/java/batch/JpaOrderServiceTest.java
+++ spring-jdbc-batch-insert/src/test/java/batch/JpaOrderServiceTest.java
@@ -1,27 +1,43 @@
 package batch;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testcontainers.shaded.com.google.common.collect.Lists;
 
+/**
+ * 삽입 완료 후, 엔티티 식별자가 매핑되어야 한다.
+ * 하지만 bulk insert는 모든 엔티티의 식별자를 알 수 없다.
+ * 따라서 row별로 write 쿼리가 발생한다.
+ */
 class JpaOrderServiceTest extends TestContainerSupport {
 
+    @Autowired
+    private JpaOrderService jpaOrderService;
+
     @Test
-    @DisplayName("sample")
-    void sample() {
-        // given
+    @DisplayName("jpa로 order 데이터 10만개를 저장한다. (orderLineItem 미포함)")
+    void createWithOutItem() {
+        List<OrderRequest> orderRequests = OrderRequestGenerator.generateRequestsWithoutItem();
 
-        // when
-
-        // then
+        jpaOrderService.createOrder(orderRequests);
     }
 
     @Test
-    @DisplayName("sample")
-    void sample2() {
-        // given
+    @DisplayName("jpa로 order 데이터 10만개를 저장한다. (orderLineItem 포함)")
+    void createWithOutDivide() {
+        List<OrderRequest> orderRequests = OrderRequestGenerator.generateRequests();
 
-        // when
+        jpaOrderService.createOrder(orderRequests);
+    }
 
-        // then
+    @Test
+    @DisplayName("jpa로 order 데이터 10만개를 분할해서 저장한다. (orderLineItem 포함)")
+    void createWithDivide() {
+        List<OrderRequest> orderRequests = OrderRequestGenerator.generateRequests();
+        List<List<OrderRequest>> partition = Lists.partition(orderRequests, 1000);
+
+        partition.forEach(jpaOrderService::createOrder);
     }
 }
---
+++ spring-jdbc-batch-insert/src/test/java/batch/OrderRequestGenerator.java
@@ -0,0 +1,53 @@
+package batch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
+import org.testcontainers.shaded.org.apache.commons.lang3.RandomUtils;
+
+class OrderRequestGenerator {
+
+    private static final Long REQUEST_COUNT = 100_000L;
+
+    public static List<OrderRequest> generateRequests() {
+        List<OrderRequest> orderRequests = new ArrayList<>();
+
+        for (int i = 0; i < REQUEST_COUNT; i++) {
+            List<OrderLineItemRequest> orderLineItemRequests = new ArrayList<>();
+
+            for (int j = 0; j < RandomUtils.nextInt(1, 5); j++) {
+                OrderLineItemRequest itemRequest = new OrderLineItemRequest(
+                        RandomUtils.nextLong(1, 500),
+                        RandomUtils.nextLong(1, 500)
+                );
+
+                orderLineItemRequests.add(itemRequest);
+            }
+
+            OrderRequest orderRequest = new OrderRequest(
+                    RandomStringUtils.randomAlphabetic(6),
+                    orderLineItemRequests
+            );
+
+            orderRequests.add(orderRequest);
+        }
+
+        return orderRequests;
+    }
+
+    public static List<OrderRequest> generateRequestsWithoutItem() {
+        List<OrderRequest> orderRequests = new ArrayList<>();
+
+        for (int i = 0; i < REQUEST_COUNT; i++) {
+            OrderRequest orderRequest = new OrderRequest(
+                    RandomStringUtils.randomAlphabetic(6),
+                    Collections.emptyList()
+            );
+
+            orderRequests.add(orderRequest);
+        }
+
+        return orderRequests;
+    }
+}

c476f18b925926020d48c970ad37ffeb0cc40c6c
le2sky <leehaneul990623@gmail.com>
2024-08-29 21:40:30 +0900

    chore: cors 설정

---
+++ pet-maeil-mail/src/main/java/maeilmail/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package maeilmail;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .allowedOrigins("https://maeil-mail.vercel.app");
+    }
+}

040957cbd1a5770cc83b470408e365417223a2a3
le2sky <leehaneul990623@gmail.com>
2024-08-31 14:41:11 +0900

    fix: ddl 변경

--- pet-maeil-mail/src/main/resources/ddl.sql
+++ pet-maeil-mail/src/main/resources/ddl.sql
@@ -1,7 +1,7 @@
 create table question
 (
     id       bigint auto_increment,
-    content  varchar(255) not null,
+    content  text not null,
     title    varchar(255) not null,
     category enum ('BACKEND','FRONTEND') not null,
     primary key (id)

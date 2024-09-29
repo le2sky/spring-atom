805dde63acf79c5b1c85654fae7f16dfcecf129b
le2sky <leehaneul990623@gmail.com>
2024-09-29 21:23:43 +0900

    feat: 비관적락 예제 구현

--- spring-concurrency-coupon/coupon-pessimistic-lock/src/main/java/coupon/domain/MemberCouponRepository.java
+++ spring-concurrency-coupon/coupon-pessimistic-lock/src/main/java/coupon/domain/MemberCouponRepository.java
@@ -1,8 +1,14 @@
 package coupon.domain;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 
 public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "10000")})
     boolean existsMemberCouponByMemberIdAndCouponId(Long memberId, Long couponId);
 }

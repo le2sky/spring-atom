150d2ecc3d948432976a32e624ef2007c67d3d7f
le2sky <leehaneul990623@gmail.com>
2024-10-01 18:03:47 +0900

    refactor: rettuce lock 구현체 로직 변경

--- spring-concurrency-coupon/coupon-distributed-lock-lettuce/src/main/java/coupon/infrastructure/lock/RettuceMemberCouponIssueLock.java
+++ spring-concurrency-coupon/coupon-distributed-lock-lettuce/src/main/java/coupon/infrastructure/lock/RettuceMemberCouponIssueLock.java
@@ -13,7 +13,29 @@ class RettuceMemberCouponIssueLock implements MemberCouponIssueLock {
     private final StringRedisTemplate redisTemplate;
 
     @Override
-    public boolean lock(Long memberId, Long couponId) {
+    public void lock(Long memberId, Long couponId) {
+        int tryCount = 10;
+
+        tryLockWithSpin(memberId, couponId, tryCount);
+    }
+
+    private void tryLockWithSpin(Long memberId, Long couponId, int tryCount) {
+        while (!requestLock(memberId, couponId)) {
+            if (tryCount-- == 0) {
+                // lock 획득 실패 처리
+                throw new RuntimeException();
+            }
+
+            try {
+                // redis에 너무 많은 부하를 주지 않기 위해 sleep을 설정
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private boolean requestLock(Long memberId, Long couponId) {
         Boolean result = redisTemplate
                 .opsForValue() // opsForX는 커맨드를 호출할 수 있는 기능을 모은 인터페이스를 반환
                 .setIfAbsent(generateKey(memberId, couponId), "empty", Duration.ofSeconds(3));

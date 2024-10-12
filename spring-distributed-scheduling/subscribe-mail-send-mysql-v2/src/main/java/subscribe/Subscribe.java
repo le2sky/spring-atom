ee2a724fc457319176b4b996c0af2f4a532a969d
le2sky <leehaneul990623@gmail.com>
2024-10-13 01:21:50 +0900

    init: subscribe mail send mysql v2 예제 초기화

---
+++ spring-distributed-scheduling/subscribe-mail-send-mysql-v2/src/main/java/subscribe/Subscribe.java
@@ -0,0 +1,16 @@
+package subscribe;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+class Subscribe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}

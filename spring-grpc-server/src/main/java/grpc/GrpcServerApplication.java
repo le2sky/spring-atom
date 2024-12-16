8783584261e099b6f4d7ea1df30356567f194b71
le2sky <leehaneul990623@gmail.com>
2024-12-16 16:03:51 +0900

    init: grpc 프로젝트 초기 구성

---
+++ spring-grpc-server/src/main/java/grpc/GrpcServerApplication.java
@@ -0,0 +1,12 @@
+package grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class GrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+}
